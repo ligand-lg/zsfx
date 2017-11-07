81c26ec339b89a03569036c6e0ed5ed48c2f04af
lab-ubuntu <lhang1995@gmail.com>
2017-11-07 21:54:42 +0800

    stepwise

--- homework/stepwise.py
+++ homework/stepwise.py
@@ -2,8 +2,8 @@
 from sklearn.feature_selection import SelectPercentile
 from sklearn.feature_selection import f_regression
 from sklearn.linear_model import Ridge
-from homework import conf_hw
-#import conf_hw
+#from homework import conf_hw
+import conf_hw
 import numpy as np
 import time
 
@@ -11,14 +11,17 @@ import time
 with open('../data/predict_stepwise.txt', 'wt', encoding='utf-8') as fout:
     start = time.time()
     start_in = time.time()
-    for qid in range(201, 202):
+
+    for qid in range(201, 251):
         print(qid)
         x_train, y_train = conf_hw.read_train(qid)
         x_test, y_test = conf_hw.read_test(qid)
+
         # 截距
         x_train = np.column_stack((x_train, np.ones((x_train.shape[0], 1))))
         x_test = np.column_stack((x_test, np.ones((x_test.shape[0], 1))))
         # 特征选择
+
         stepwise = SelectPercentile(f_regression, percentile=0.7)
         stepwise.fit(x_train, y_train)
         x_train = stepwise.transform(x_train)
@@ -37,14 +40,14 @@ with open('../data/predict_stepwise.txt', 'wt', encoding='utf-8') as fout:
         for relationship in conf_hw.coll_test.find({'query_id': str(qid)}):
             article_id = relationship['article_id']
             id_code = relationship['id_code']
-            score = y_hat[i]
+            score = y_hat[i,0]
             fout.write('{0} Q0 {1} {2} {3} Hiemstra_LM0.15_Bo1bfree_d_3_t_10\n'.format(qid, article_id, id_code, score))
             i += 1
-    conf_hw.MAE('mars')
+    conf_hw.MAE('stepwise')
     end = time.time()
     print('total : {0}'.format((end-start)/60))
 
-
+# MAE: 2.1939343167333814
 
 
 
