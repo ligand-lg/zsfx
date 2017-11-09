07967d52e763c69da130d184947b11d22483f14d
lab-ubuntu <lhang1995@gmail.com>
2017-11-09 08:59:14 +0800

    homework work

--- homework/loess.py
+++ homework/loess.py
@@ -1,10 +1,10 @@
 # coding:utf-8
 
 import numpy as np
-from homework import conf_hw
-#import conf_hw
+#from homework import conf_hw
+import conf_hw
 from sklearn.decomposition import PCA
-
+import time
 
 def loess(test_point, x_mat, y_mat):
     m, n = x_mat.shape
@@ -13,7 +13,7 @@ def loess(test_point, x_mat, y_mat):
     dists = []
     for i in range(m):
         diff_mat = test_point - x_mat[i, :]
-        dists.append((diff_mat*diff_mat.T)[0, 0])
+        dists.append((diff_mat*diff_mat.T)[0])
     max_dist = max(dists)
     for j in range(m):
         weight_mat[j, j] = (1-(dists[j]/max_dist)**3)**3
@@ -31,8 +31,10 @@ def loess(test_point, x_mat, y_mat):
 
 
 if __name__ == '__main__':
+    start = time.time()
     with open('../data/predict_loess.txt', 'wt', encoding='utf-8') as fout:
-        for qid in range(201, 202):
+        for qid in range(201, 251):
+            print(qid)
             train_data, train_lab = conf_hw.read_train(qid)
             test_data, test_lab = conf_hw.read_test(qid)
             test_r, test_c = test_data.shape
@@ -40,16 +42,20 @@ if __name__ == '__main__':
             train_data = np.column_stack((train_data, np.ones((train_data.shape[0], 1))))
             test_data = np.column_stack((test_data, np.ones((test_data.shape[0], 1))))
             # PCA 降维
+            print('PCA...')
+            start_in = time.time()
             pca = PCA(n_components=300)
             train_data = pca.fit_transform(train_data)
             test_data = pca.transform(test_data)
-
+            print('PCA: {0}'.format(time.time()-start_in))
+            start_in = time.time()
+            print('test...')
             predicts = []
             for r in range(test_r):
                 predict_lab = loess(test_data[r, :], train_data, train_lab)
                 predicts.append(predict_lab[0, 0])
                 print(r)
-
+            print('test end :{0}'.format(time.time()-start_in))
             # write to file
             i = 0
             for relationship in conf_hw.coll_test.find({'query_id': str(qid)}):
