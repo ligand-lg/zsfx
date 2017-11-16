c818360cdaef292585ee06caedfda4781f4a8056
lab-ubuntu <lhang1995@gmail.com>
2017-11-16 16:16:30 +0800

    lvq

--- homework/lvq.py
+++ homework/lvq.py
@@ -4,12 +4,13 @@ import numpy as np
 
 
 def lvq():
+    print('start')
     error = 0
     total = 0
     for qid in range(201, 251):
         qid = str(qid)
         X_train, Y_train = conf_hw.read_train(qid, type='class')
-        X_test, Y_test = conf_hw.read_test('201', type='class')
+        X_test, Y_test = conf_hw.read_test(qid, type='class')
         model = glvq.GlvqModel()
         model.fit(X_train, np.array(Y_train).ravel())
         y_hats = model.predict(X_test)
@@ -26,6 +27,7 @@ def lvq():
                     '{0} Q0 {1} {2} {3} Hiemstra_LM0.15_Bo1bfree_d_3_t_10\n'.format(qid, article_id, id_code, score))
                 i += 1
     print('error ratio: {0}'.format(error/total))
+    print('end')
 
-if '__name__' == '__main__':
+if __name__ == '__main__':
     lvq()
